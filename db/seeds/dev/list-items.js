8b9274346cdd699e77c90be64a25950ed1ee0dfd
Arram Mandel <airum82@gmail.com>
2018-09-04 09:49:25 -0600

    Add knex to server file and completed get endpoint

--- db/seeds/dev/list-items.js
+++ db/seeds/dev/list-items.js
@@ -5,9 +5,8 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('list_items').insert([
-        {id: 1, title: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {id: 1, title: 'Deep South Road Trip', description: 'Take a road trip through the deep south'},
+        {id: 2, title: 'Kendo', description: 'Learn how to fight with a katana'}
       ]);
     });
 };
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const environment = process.env.NODE_ENV ||
+'development';
+const configuration = require('./knexfile')
+[environment];
+const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
 
@@ -8,7 +13,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/v1/bucket-items', (request, response) => {
-  return response.json('oh hello');
+  database('list_items').select()
+    .then(listItems => {
+      return response.status(200).json(listItems);
+    })
 });
 
 app.post('/api/v1/new-item', (request, response) => {
@@ -16,7 +24,7 @@ app.post('/api/v1/new-item', (request, response) => {
 });
 
 app.delete('api/v1/remove/:item', (request, response) => {
-  
+
 });
 
 app.listen(app.get('port'), () => {
